0b5f5ab368390d672b57a97c4988bcc8d867f790
alcubierre-drive <alcubierre-drive>
2021-06-30 14:53:40 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 sudo cp src/xtrlock-pam /usr/bin/
-sudo cp src/40-libfprint-vfs0090-custom.rules /lib/udev/rules.d/
-sudo cp src/xtrlock-pam-auth /etc/pam.d/
 
 echo "
 You might want the following in the file ´/etc/systemd/system/suspend@.service´:
