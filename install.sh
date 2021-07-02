00e3aa7adc0d2082dcc56e9f8d2e2e7a88c4f98e
alcubierre-drive <alcubierre-drive>
2021-07-02 20:22:07 +0200

    install script with prefix

--- install.sh
+++ install.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-sudo cp src/xtrlock-pam /usr/bin/
-
-echo "
-You might want the following in the file ´/etc/systemd/system/suspend@.service´:
-
-´´´
-[Unit]
-Description=User suspend actions
-Before=suspend.target
-
-[Service]
-User=lennart
-Type=simple
-Environment=DISPLAY=:0
-ExecStart=+/usr/bin/xtrlock-pam -s
-
-[Install]
-WantedBy=suspend.target
-´´´
-"
+install -m 755 src/xtrlock-pam $PREFIX/usr/bin/
