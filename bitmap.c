37410bdf50019b02312ca214cd9492cd04b6b2e9
alcubierre-drive <alcubierre-drive>
2021-07-22 11:04:56 +0200

    bitmap reader, now missing edge detection

--- bitmap.c
+++ bitmap.c
@@ -30,6 +30,33 @@ typedef struct {
     unsigned char i7:1;
 } bitwise_pixels;
 
+static void bitwise_pixels_true( bitwise_pixels* pxl, int idx ) {
+    if (idx==0) pxl->i0=1;
+    if (idx==1) pxl->i1=1;
+    if (idx==2) pxl->i2=1;
+    if (idx==3) pxl->i3=1;
+    if (idx==4) pxl->i4=1;
+    if (idx==5) pxl->i5=1;
+    if (idx==6) pxl->i6=1;
+    if (idx==7) pxl->i7=1;
+}
+
+static void bitwise_pixels_false( bitwise_pixels* pxl, int idx ) {
+    if (idx==0) pxl->i0=0;
+    if (idx==1) pxl->i1=0;
+    if (idx==2) pxl->i2=0;
+    if (idx==3) pxl->i3=0;
+    if (idx==4) pxl->i4=0;
+    if (idx==5) pxl->i5=0;
+    if (idx==6) pxl->i6=0;
+    if (idx==7) pxl->i7=0;
+}
+
+static void bitwise_pixels_set( bitwise_pixels* pxl, int idx, int val ) {
+    if (val) bitwise_pixels_true( pxl, idx );
+    else bitwise_pixels_false( pxl, idx );
+}
+
 void xtrlock_bitmap_read( const char* file ) {
     if (file == NULL) {
         lock_bits = _lock_bits;
@@ -43,10 +70,9 @@ void xtrlock_bitmap_read( const char* file ) {
     } else {
         unsigned int width, height;
         unsigned char *pixels = NULL;
-        unsigned int x_hot, y_hot;
+        int x_hot, y_hot;
         int err = XReadBitmapFileData( file, &width, &height, &pixels, &x_hot, &y_hot );
 
-        int nbytes = ( width/8 + (width%8>=1) ) * height;
         lock_bits = pixels;
         lock_width = width;
         lock_height = height;
@@ -57,6 +83,14 @@ void xtrlock_bitmap_read( const char* file ) {
         mask_bits = pixels;
         mask_width = width;
         mask_height = height;
+
+        int nbytes_per_row = width/8 + (width%8>=1);
+        for (int y=0; y<height; ++y) for (int x=0; x<width; ++x) {
+            int byte_idx = x/8 + y*nbytes_per_row;
+            int bit_idx = x%8;
+            int pixel_on = (x==0 || x==width-1 || y==0 || y==height-1);
+            bitwise_pixels_set( (bitwise_pixels*)(mask_bits+byte_idx), bit_idx, !pixel_on );
+        }
 
         if (!err) free_memory = 1;
         else xtrlock_bitmap_read(NULL);
